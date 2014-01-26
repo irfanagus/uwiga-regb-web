be849376acdd2486b452d64df0db5916248d537b
irfan <irfan@irfan-PC>
2014-01-26 10:22:27 +0700

    penanda pertama

--- src/textgitjava/TextGitJava.java
+++ src/textgitjava/TextGitJava.java
@@ -15,5 +15,6 @@ public class TextGitJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("tulisan yang pertama");
     }
 }
